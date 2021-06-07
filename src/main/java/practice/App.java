fdda51c6f1931b608b3c946d79821f25d3f2d908
sangatiupendrareddy <sangatiupendrareddys123@gmail.com>
2021-06-07 14:27:05 +0000

    soo

---
+++ src/main/java/practice/App.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class App {
+    public static void main(String... args) {
+        System.out.println("Hi from featureflow - replace me with something more useful - check the tutorial at www.featureflow.io :)");
+    }
+}
